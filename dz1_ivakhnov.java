b0775e191890a08c8d93bade62dd5a61c8eebe52
SanyaDataEngineer <{xameleonfirst@gmail.com}>
2025-02-09 22:27:53 +0300

    1 дз, 1 задание

---
+++ dz1_ivakhnov.java
@@ -0,0 +1,12 @@
+public class dz1_ivakhnov {
+    public static void main(String[] args) {
+        greetings();
+    }
+
+    public static void greetings() {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
+    }
+}
